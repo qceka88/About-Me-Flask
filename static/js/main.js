894bbce387fb8e6b5b78ad1588c53d14d1136801
Yanko Rashkov <thewhitedead@abv.bg>
2023-10-25 20:22:31 +0300

    added chat window

--- static/js/main.js
+++ static/js/main.js
@@ -10,8 +10,8 @@
         }, 1);
     };
     spinner();
-    
-    
+
+
     // Initiate the wowjs
     new WOW().init();
 
@@ -30,19 +30,19 @@
     $(".navbar-nav a").on('click', function (event) {
         if (this.hash !== "") {
             event.preventDefault();
-            
+
             $('html, body').animate({
                 scrollTop: $(this.hash).offset().top - 45
             }, 1000, 'easeInOutExpo');
-            
+
             if ($(this).parents('.navbar-nav').length) {
                 $('.navbar-nav .active').removeClass('active');
                 $(this).closest('a').addClass('active');
             }
         }
     });
-    
-    
+
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
@@ -69,7 +69,7 @@
         autoplay: true,
         smartSpeed: 1000,
         loop: true,
-        dots:false,
+        dots: false,
         autoplayTimeout: 3000,
         items: 1
     });
@@ -84,22 +84,31 @@
         dots: false,
         nav: true,
         page: true,
-        navText : [
+        navText: [
             '<i class="bi bi-chevron-left"></i>',
             '<i class="bi bi-chevron-right"></i>'
         ],
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            768:{
-                items:2
+            768: {
+                items: 2
             },
-            992:{
-                items:3
+            992: {
+                items: 3
             }
         }
     });
-    
+
+
 })(jQuery);
 
+function toggleForm() {
+    var form = document.getElementById("myForm");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
+}
