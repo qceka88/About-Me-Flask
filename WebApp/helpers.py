1aadfe5b2c30f56731d8b8be294c364b67ba2a35
Yanko Rashkov <thewhitedead@abv.bg>
2024-01-18 16:27:24 +0200

    Fixed bug

--- WebApp/helpers.py
+++ WebApp/helpers.py
@@ -5,35 +5,25 @@ from flask import request
 
 
 class InfoClass:
-
-    @property
-    def information(self):
-        data = """I'm a junior in programming, driven by a deep passion and a constant eagerness to learn more about 
+    information = """I'm a junior in programming, driven by a deep passion and a constant eagerness to learn more about 
         this science, utilizing it to develop new technologies and explore new frontiers. I have a solid foundation in 
         programming and DevOps practices, aspiring to be part of this impressive and dynamic industry. Through my work 
         experience, I've developed the ability to excel both as a team player and independently, always staying focused and 
         achieving goals. The combination of a developer and a mechanical engineer brings a unique perspective and meticulous 
         attention to detail.
                 """
-        return data
-
-    @property
-    def context(self):
-        data = {
-            "CV": "/static/pdf/Yanko_CV.pdf",
-            "personal_information": self.information,
-        }
-
-        return data
+    context = {
+        "CV": "/static/pdf/Yanko_CV.pdf",
+        "personal_information": information,
+    }
 
 
 class MyDevice:
 
     @staticmethod
     def device_detect():
-        user_agent = request.headers.get("User-Agent")
-
-        if "iphone" in user_agent.lower() or "android" in user_agent.lower():
+        user_agent = request.headers.get("User-Agent").lower()
+        if "iphone" in user_agent or "android" in user_agent:
             return True
 
         return False
