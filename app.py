f708c10b19679c5ce67eba1f93f62a8c40094ae1
Yanko Rashkov <thewhitedead@abv.bg>
2024-03-17 18:33:35 +0200

    fix forgotten comment

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from keras.models import load_model
 
 from ChatBot.chat_bot_actions import BotResponse
 from WebApp.helpers import unique_extension
-from WebApp.views import IndexView, HobbiesView#, PageNotFoundView, BotResponseView
+from WebApp.views import IndexView, HobbiesView, Page404View, BotResponseView
 
 nltk.download("popular")
 nltk.download("omw-1.4")
@@ -34,7 +34,7 @@ app.config["SECRET_KEY"] = "FlasK/Pythonfi234j2fr2j-939jsdf2sdf43243a2"
 app.add_url_rule("/", view_func=IndexView.as_view("index_view", "index.html"))
 app.add_url_rule("/get", view_func=BotResponseView.as_view("bot_response", bot))
 app.add_url_rule(f"/{unique_extension}", view_func=HobbiesView.as_view("hobbies_view", "hobbies.html"))
-app.add_url_rule("/<path:invalid_path>", view_func=PageNotFoundView.as_view("page_not_found"))
+app.add_url_rule("/<path:invalid_path>", view_func=Page404View.as_view("page_not_found"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=False)
